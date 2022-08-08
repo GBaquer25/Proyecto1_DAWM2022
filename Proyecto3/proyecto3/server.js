c9197afbadc5ec905129589643c09da585064118
Génesis Baquerizo <gdbaquer@espol.edu.ec>
2022-08-08 00:54:43 -0500

    heroku2

--- Proyecto3/proyecto3/server.js
+++ Proyecto3/proyecto3/server.js
@@ -2,10 +2,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 // Serve static files
-app.use(express.static(__dirname + '/dist/sitio'));
+app.use(express.static(__dirname + '/dist/proyecto3'));
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-   res.sendFile(path.join(__dirname + '/dist/sitio/index.html'));
+   res.sendFile(path.join(__dirname + '/dist/proyecto3/index.html'));
 });
 // default Heroku port
 app.listen(process.env.PORT || 5000);
